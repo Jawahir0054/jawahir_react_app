4624dba01297e2edf17e52b84ed04bbde7b66c75
Jawahir0054 <jawahiriyaahhmed@gmail.com>
2025-06-08 21:56:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import header from './components/header';
+
+function App() {
+  
+  return (
+    <>
+    <Greeting name="Jawahir" Surname="Shariff" />
+
+     <Header />
+     <Header />
+     <Header />
+     
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+        <h1>This is my first React Application</h1>
+    </div>
+
+  )
+}
+
+export default header
